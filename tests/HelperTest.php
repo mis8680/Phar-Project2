93529b8eb0f1ec6cbecb8dfbbbb9a3491adb57c9
Rawrlie <rawrlie@ubuntu.ubuntu-domain>
2013-04-13 23:23:24 -0400

    test classes

---
+++ tests/HelperTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Namespace 
+ */
+
+/**
+ * HelperTest class extending PHPUnit_Framework_TestCase
+ *
+ * @package
+ *
+ * @author Jeremy Mills
+ * @author Insu Mun
+ * @author Carlie Hiel
+ *
+ * @copyright 2013
+ * @version PHP 5.3
+ */
+class HelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * testExecCommand tests execCommand() of Helper class
+     *
+     * @access public
+     */
+    public function testExecCommand() {
+        
+    }
+    
+    /**
+     * testGopt tests gopt() of Helper class
+     *
+     * @access public
+     */
+    public function testGopt() {
+        
+    }
+    
+    /**
+     * testGlopt tests glopt() of Helper class
+     *
+     * @access public
+     */
+    public function testGlopt() {
+        
+    }
+    
+     /**
+     * testReqopt tests reqopt() of Helper class
+     *
+     * @access public
+     */
+    public function testReqopt() {
+        
+    }
+    
+     /**
+     * testError tests error() of Helper class
+     *
+     * @access public
+     */
+    public function testError() {
+        
+    }
+    
+     /**
+     * testSuccess tests success() of Helper class
+     *
+     * @access public
+     */
+    public function testSuccess() {
+        
+    }
+    
+     /**
+     * testMakeAbsolut tests makeAbsolut() of Helper class
+     *
+     * @access public
+     */
+    public function testMakeAbsolut() {
+        
+    }
+    
+}
