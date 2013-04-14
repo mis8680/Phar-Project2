2dff35f49bc07b61cca1e7e428fda397f5a0e1e2
mis8680 <mis8680@mis8680-virtual-machine.(none)>
2013-04-14 14:17:51 -0400

    clean up some files

---
+++ run.php
@@ -0,0 +1,4 @@
+<?php 
+
+require "phar://example.phar/bin/index.php";
+
--- src/PharCompiler/Console/Make.php
+++ src/PharCompiler/Console/Make.php
@@ -11,31 +11,19 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use \Phar;
 
-
 class Make extends Command
 {   
     public function __construct()
     {
         parent::__construct();
-	
     }
     
     protected function configure()
     {
-	
         //set the name of the command
         $this
 	    ->setName('make')
 	    ->setDescription('Create new .phar file')
-<<<<<<< HEAD
-	    ->setDefinition(array(
-                new InputArgument('phar_file',null,"This is the name of your future phar file,\nyou can pass absolute or relative path,\nbut don't forget the extension."),
-                new InputArgument('root_app',null,"This is the root directory of your application\nyou want to turn into phar, you can pass absolute or\nrelative path."),
-             ))
-	    
-	    
-	;
-=======
 	    ->setDefinition(
 		array(
 		    new InputArgument('phar_file',null,"this is the name of your future phar file,\nyou can pass absolute or relative path,\nbut don't forget the extension."),
@@ -43,88 +31,10 @@ class Make extends Command
 		    new InputArgument('root_app',null,"this is the root directory of your application\nyou want to turn into phar, you can pass absolute or\nrelative path."),
 		)
 	    );
->>>>>>> 1f5b012cf183bc3f2691836d50905ed113f6948e
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-<<<<<<< HEAD
-	//check if the phar file is writable
-	$this->is_phar_writable();
-	//set the phar file
-	$phar = $this->makeAbsolute($this->request_option(0, 'phar filename'));
-	//set the phar name
-	$phar_name = end(explode('/', $phar));
-	//set the phar stub file to be run
-	$stub_file = trim($this->request_option(1, 'stub file'), '/');
-	//set the root app where phar will be created from
-	$root_app = $this->makeAbsolute($this->request_option(2, 'root dir of your app'));
-	if (!file_exists($root_app)) {
-			return $this->error("Root dir of your app doesn't exist.");
-		}
-	
-	
-	
-        $output->writeln("Hello World, this is my first console program.");
-        $output->writeln('The phar file name is ..."' . $input->getArgument('phar_file') . '"');
-	$output->writeln('The root app is ..."' . $input->getArgument('root_app') . '"');
-              
-        $output->writeln(PHP_EOL . PHP_EOL);
-    }
-    
-    protected function is_phar_writable()
-    {
-		if (!Phar::canWrite()) {
-			exit($this->error(
-				"Unable to write phar, phar.readonly must be set to zero in your php.ini otherwise use:
-				$ php -dphar.readonly=0 empir <command> ...")
-			);
-	    }
-    }
-    
-    protected function makeAbsolute($path = '')
-    {
-		$current = getcwd().'/';
-		if ($path === "" || $path === false) {
-			$absolut_path = $current;
-		} elseif (substr($path, 0, 2) == './') {
-			$absolut_path = $current.substr($path, 2);
-		} elseif (strpos($path, ':') === 1 || substr($path, 0, 2) == '\\\\' || substr($path, 0, 1) == '/') {
-			$absolut_path = $path;
-		} else {
-			$absolut_path = $current.$path;
-		}
-
-		$absolut_path = str_replace('\\', '/', $absolut_path);
-		$absolut_path = rtrim($absolut_path, '/');
-
-		return $absolut_path;
-    }
-    protected function get_option($no)
-    {
-		if (isset($this->options[$no])) {
-			return $this->options[$no];
-		}
-		return null;
-    }
-
-    protected function request_option($no, $name)
-    {
-	if ($this->get_option($no) == null) {
-			exit($this->error("Param $name is required. Try <help>"));
-		}
-		return $this->get_option($no);
-    }
-    
-    protected function error($message = '', $errno = 1)
-    {
-	if ($message != '') {
-			echo Color::str("ERROR: $message\n", Empir::ERR_COLOR);
-		}
-		return $errno;
-    }
-    
-=======
 	$output->writeln("\nThe phar_file name is ..." . $input->getArgument('phar_file'));
 	$output->writeln('The phar_stub name is ..."' . $input->getArgument('phar_stub') . '"');
 	$output->writeln('The root_app is ..."' . $input->getArgument('root_app') . '"');
@@ -175,19 +85,19 @@ class Make extends Command
 
 	//do not show error message when unlinking $phar
 	@unlink($phar);
-		
+
 	//perform compression
 	//catch exception thrown and display to user if error occurs
 	try {
 	    $p = new Phar($phar, Phar::CURRENT_AS_FILEINFO | Phar::KEY_AS_FILENAME, $phar_name);
-	    
+
 	    echo "\nCompressing files into: ".$phar_name;
 	    echo "\nMaking babies.. \n===================\n";
-	
+
 	    $p->setStub("<?php Phar::mapPhar(); include 'phar://".$phar_name."/".$stub_file."'; __HALT_COMPILER(); ?>");
 
 	    $files = $this->_scandir($root_app);
-	    
+
 	    // counter variable to display the number of files that will be added
 	    $count = 0;
 	    foreach ($files as $file) {
@@ -203,7 +113,7 @@ class Make extends Command
 	    }
 
 	    echo "\nTotal: $count files added\n";
-	    
+
 	    return $this->success("CREATED $phar.. thank you for using:
  _______    _____      _   __	  _   __        _
 |__   __|  / ___ \    | | / /    | | / /       / \
@@ -218,13 +128,13 @@ class Make extends Command
 Jokka is a complete symfony based phar compiling tool.
 Jokka is built with the help of (c) Jeremy Perret's <jeremy@devstar.org> Empir php compiling tool.
 phar.readonly must be set to 0 within your php.ini in order to run");
-	
+
 	    $phar_copy = $phar.'phar';
 	    @unlink($phar_copy);
 	    $p = $p->convertToExecutable(Phar::PHAR, Phar::NONE);
 	    $this->success("CREATE $phar_copy");
 	    @unlink($phar);
-		
+
 	} catch (Exception $e) {
 		return $this->error($e->getMessage());
 	}
@@ -241,7 +151,6 @@ phar.readonly must be set to 0 within your php.ini in order to run");
 	    );
 	}
     }
->>>>>>> 1f5b012cf183bc3f2691836d50905ed113f6948e
     
     /**
      *
@@ -342,7 +251,7 @@ phar.readonly must be set to 0 within your php.ini in order to run");
 	if ($message != '') {
 	    //create a new output instance variable
 	    $output = new ConsoleOutput();
-	    
+
 	    $output->writeln("<error>ERROR: $message\n</error>");
 	}
 	return $errno;
@@ -355,7 +264,7 @@ phar.readonly must be set to 0 within your php.ini in order to run");
     {
 	//create a new output instance variable
 	$output = new ConsoleOutput();
-	
+
 	$output->writeln("<info>$message\n</info>");
     }
     
@@ -366,7 +275,7 @@ phar.readonly must be set to 0 within your php.ini in order to run");
     {
 	$items = array();
 	$path = rtrim($path, '/');
-	
+
 	if (!$current_dir = opendir($path)) {
 		return $items;			
 	}
