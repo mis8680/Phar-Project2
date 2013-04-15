06bc9af6a80a7803d7f73e73c07e56a01e533ada
mis8680 <mis8680@mis8680-virtual-machine.(none)>
2013-04-15 10:56:46 -0400

    modified make file

--- src/PharCompiler/Console/Make.php
+++ src/PharCompiler/Console/Make.php
@@ -34,11 +34,7 @@ class Make extends Command
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
-    {
-	$output->writeln("\nThe phar_file name is ..." . $input->getArgument('phar_file'));
-	$output->writeln('The phar_stub name is ..."' . $input->getArgument('phar_stub') . '"');
-	$output->writeln('The root_app is ..."' . $input->getArgument('root_app') . '"');
-        
+    {   
 	$this->compress($input->getArgument('phar_file'), $input->getArgument('phar_stub'), $input->getArgument('root_app'));
         
         $output->writeln(PHP_EOL . PHP_EOL);
@@ -55,12 +51,19 @@ class Make extends Command
 	//set the phar stub file to be run
 	$stub_file = trim($stub);
 	//set the root app where phar will be created from
-	$root_app = $root;	
+	$root_app = $root;
 
+	//Check if the root_app actually exists
 	if (!file_exists($root_app)) {
 		return $this->error("Root dir of your app doesn't exist.");
 	}
 
+	//Check if the stub file actually exists
+	if (!$this->stub_exists($root_app,$this->_scandir($root_app),$stub_file)) {
+	    return $this->error("Stub file ".$stub_file." doesn't exist within ".$root_app);
+	}
+
+	//unlink previous phar if exists
 	//do not show error message when unlinking $phar
 	@unlink($phar);
 
@@ -69,7 +72,7 @@ class Make extends Command
 	try {
 	    $p = new Phar($phar, Phar::CURRENT_AS_FILEINFO | Phar::KEY_AS_FILENAME, $phar_name);
 
-	    echo "\nCompressing files into: " . $phar_name;
+	    echo "\nCompressing files into: ".$phar_name;
 	    echo "\nMaking babies.. \n===================\n";
 
 	    $p->setStub("<?php Phar::mapPhar(); include 'phar://".$phar_name."/".$stub_file."'; __HALT_COMPILER(); ?>");
@@ -78,21 +81,20 @@ class Make extends Command
 
 	    // counter variable to display the number of files that will be added
 	    $count = 0;
+
 	    foreach ($files as $file) {
 		$file_buff = $file;
 		$file = str_replace('\\', '/', $file);
 		$file = str_replace($root_app.'/', '', $file);
 
-		//if (!$this->_exclude($file, $shell_masks) && !$this->_exclude($file, array('*/'.$phar_name, $phar_name))) {
-		    $p[$file] = file_get_contents($file_buff);
-		    echo "adding $file ..\n";
-		    $count++;
-		//}
+		$p[$file] = file_get_contents($file_buff);
+		echo "adding $file ..\n";
+		$count++;
 	    }
 
 	    echo "\nTotal: $count files added\n";
 
-	    return $this->success("CREATED $phar.. thank you for using:
+	    return $this->success("CREATED $phar.. \nthank you for using:
  _______    _____      _   __	  _   __        _
 |__   __|  / ___ \    | | / /    | | / /       / \
    | |	  / /   \ \   | |/ /     | |/ /       / / \
@@ -105,13 +107,7 @@ class Make extends Command
 (c) Carlie Hiel <carlie.hiel@gmail.com>
 Jokka is a complete symfony based phar compiling tool.
 Jokka is built with the help of (c) Jeremy Perret's <jeremy@devstar.org> Empir php compiling tool.
-phar.readonly must be set to 0 within your php.ini in order to run");
-
-	    $phar_copy = $phar.'phar';
-	    @unlink($phar_copy);
-	    $p = $p->convertToExecutable(Phar::PHAR, Phar::NONE);
-	    $this->success("CREATE $phar_copy");
-	    @unlink($phar);
+");
 
 	} catch (Exception $e) {
 		return $this->error($e->getMessage());
@@ -130,7 +126,18 @@ phar.readonly must be set to 0 within your php.ini in order to run");
 	}
     }
     
-    
+    /**
+     *
+     */
+    private function stub_exists($root, $scanned_dir, $stub_file)
+    {
+	foreach ($scanned_dir as $handle) {
+	    if ($root .'/'. $stub_file == $handle) {
+		return TRUE;
+	    }
+	}
+	return FALSE;
+    }
     
     /**
      *
